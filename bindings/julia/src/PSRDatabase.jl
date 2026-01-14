3ea94fed8988d8666e0a34d96687609462bdae5b
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-01-13 22:55:40 -0300

    [skip ci] update

--- bindings/julia/src/PSRDatabase.jl
+++ bindings/julia/src/PSRDatabase.jl
@@ -9,6 +9,5 @@ include("database.jl")
 
 export Element, Database, DatabaseException
 export create_empty_db_from_schema, create_element!, close!
-export read_scalar_ints, read_scalar_doubles, read_scalar_strings
 
 end
