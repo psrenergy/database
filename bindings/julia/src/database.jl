c19a85f49973ae6446bc11537e1c5a461498f78b
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-01-16 00:46:32 -0300

    update

--- bindings/julia/src/database.jl
+++ bindings/julia/src/database.jl
@@ -42,7 +42,7 @@ function close!(db::Database)
 end
 
 function set_scalar_relation!(db::Database, collection::String, attribute::String,
-                              from_label::String, to_label::String)
+    from_label::String, to_label::String)
     err = C.psr_database_set_scalar_relation(db.ptr, collection, attribute, from_label, to_label)
     if err != C.PSR_OK
         throw(DatabaseException("Failed to set scalar relation '$attribute' in '$collection'"))
