2284da9f003f2eacceaa5d89280675138d61dbe0
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-01-18 18:41:57 -0300

    update

--- bindings/julia/src/PSRDatabase.jl
+++ bindings/julia/src/PSRDatabase.jl
@@ -6,6 +6,7 @@ import .C
 include("exceptions.jl")
 include("element.jl")
 include("database.jl")
+include("database_create.jl")
 include("lua_runner.jl")
 
 export Element, Database, LuaRunner, DatabaseException
--- bindings/julia/src/database.jl
+++ bindings/julia/src/database.jl
@@ -26,26 +26,6 @@ function from_migrations(db_path, migrations_path)
     return Database(ptr)
 end
 
-function create_element!(db::Database, collection::String, e::Element)
-    if C.psr_database_create_element(db.ptr, collection, e.ptr) == -1
-        throw(DatabaseException("Failed to create element in collection $collection"))
-    end
-    return nothing
-end
-
-function create_element!(db::Database, collection::String; kwargs...)
-    e = Element()
-    for (k, v) in kwargs
-        e[String(k)] = v
-    end
-    try
-        create_element!(db, collection, e)
-    finally
-        destroy!(e)
-    end
-    return nothing
-end
-
 function close!(db::Database)
     if db.ptr != C_NULL
         C.psr_database_close(db.ptr)
