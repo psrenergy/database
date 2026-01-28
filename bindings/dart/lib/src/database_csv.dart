ad27140b4c494f30230631433f944a0836072b78
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-01-27 22:10:17 -0300

    update

--- bindings/dart/lib/src/database_csv.dart
+++ bindings/dart/lib/src/database_csv.dart
@@ -3,7 +3,7 @@ part of 'database.dart';
 /// CSV import/export operations for Database.
 extension DatabaseCSV on Database {
   /// Exports a table to a CSV file.
-  void exportToCsv(String table, String path) {
+  void exportToCSV(String table, String path) {
     _ensureNotClosed();
     final arena = Arena();
     try {
@@ -21,7 +21,7 @@ extension DatabaseCSV on Database {
   }
 
   /// Imports a CSV file into a table.
-  void importCsv(String table, String path) {
+  void importFromCSV(String table, String path) {
     _ensureNotClosed();
     final arena = Arena();
     try {
