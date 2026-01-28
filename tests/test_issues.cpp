dcb78c1798a760b6ee573b25d0fb221d6ce0d02d
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-01-28 13:06:05 -0300

    update

---
+++ tests/test_issues.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <quiver/database.h>
+#include <quiver/element.h>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+class IssuesFixture : public ::testing::Test {
+protected:
+    void SetUp() override {
+        issues_path = (fs::path(__FILE__).parent_path() / "schemas" / "issues").string();
+    }
+    std::string issues_path;
+};
+
+TEST_F(IssuesFixture, Issue52) {
+    auto migrations_path = issues_path + "/issue52";
+
+    EXPECT_THROW(
+        quiver::Database::from_migrations(":memory:", migrations_path),
+        std::runtime_error
+    );
+}
